0e5662c605244bf182377ae1e8c0fd068b3c279a
CodingIran <CodingIran@gmail.com>
2024-06-05 16:11:19 +0800

    update

--- Sources/NetworkExtensionKit/Extension/NETunnelProviderMessage.swift
+++ Sources/NetworkExtensionKit/Extension/NETunnelProviderMessage.swift
@@ -8,12 +8,15 @@
 import Foundation
 
 public enum NETunnelProviderMessageError: LocalizedError {
-    case invalidMessage
+    case tunnelProviderSessionUnavailable
+    case messageDataConvertError
 
     public var errorDescription: String? {
         switch self {
-        case .invalidMessage:
-            return "Invalid message"
+        case .tunnelProviderSessionUnavailable:
+            return "NETunnelProviderSession unavailable"
+        case .messageDataConvertError:
+            return "Message data convert error"
         }
     }
 }
@@ -66,13 +69,18 @@ import NetworkExtension
 #if swift(>=5.5)
 #if canImport(_Concurrency)
 
-public extension NETunnelProviderSession {
+public extension NETunnelProviderManager {
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    func sendProviderMessage<ResponseMessage: NETunnelProviderMessageConvertible>(_ message: NETunnelProviderMessageConvertible) async throws -> ResponseMessage? {
-        guard let messageData = message.messageData else {
-            throw NETunnelProviderMessageError.invalidMessage
+    func sendMessage<ResponseMessage: NETunnelProviderMessageConvertible>(_ message: NETunnelProviderMessageConvertible) async throws -> ResponseMessage? {
+        guard let session = connection as? NETunnelProviderSession
+        else {
+            throw NETunnelProviderMessageError.tunnelProviderSessionUnavailable
         }
-        guard let responseData = try await sendProviderMessage(messageData) else {
+
+        guard let messageData = message.messageData else {
+            throw NETunnelProviderMessageError.messageDataConvertError
+        }
+        guard let responseData = try await session.sendProviderMessage(messageData) else {
             return nil
         }
         return ResponseMessage(messageData: responseData)
